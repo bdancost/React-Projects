4543b1c2da7b1f42bff4ee3afbd334e0819586ae
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-12-07 14:51:17 -0300

    Variaçoes de tamanho de letra

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,9 +1,17 @@
 import styles from './styles.module.css'
 
+import { Letter } from '../Letter'
+
 export function LettersUsed() {
   return (
     <div className={styles.lettersUsed}>
       <h5>Letras utilizadas</h5>
+
+      <div>
+        <Letter value="A" size="small" />
+        <Letter value="B" size="small" />
+        <Letter value="C" size="small" />
+      </div>
     </div>
   )
 }
