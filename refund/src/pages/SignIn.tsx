0efeb86b79bc57f12a39d738f7782dfef891952f
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-12-12 18:00:52 -0300

    Criando Component de input

--- refund/src/pages/SignIn.tsx
+++ refund/src/pages/SignIn.tsx
@@ -1,3 +1,11 @@
+import { Input } from '../components/input'
+
 export function SignIn() {
-  return <h1>Sign In</h1>
+  return (
+    <form className="w-full flex flex-col gap-4">
+      <Input required legend="E-mail" type="email" placeholder="seu@email.com" />
+
+      <Input required legend="Senha" type="password" placeholder="123456" />
+    </form>
+  )
 }
