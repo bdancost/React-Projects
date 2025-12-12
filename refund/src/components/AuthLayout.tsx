1653968671953ac434db6ca9612567a78cdab20b
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-12-12 17:42:05 -0300

    Criando o AuthLayout

---
+++ refund/src/components/AuthLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router'
+import logoSvg from '../assets/logo.svg'
+
+export function AuthLayout() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center bg-gray-400 text-gray-100">
+      <main className="bg-gray-500 p-8 rounded-md flex items-center flex-col gap-6 md:min-w-[462px]">
+        <img src={logoSvg} alt="Logo" />
+        <Outlet />
+      </main>
+    </div>
+  )
+}
