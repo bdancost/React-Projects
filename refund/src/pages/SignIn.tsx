53790145ce76784b8790331dea9151efd038547b
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-12-13 20:45:05 -0300

    Link para SignUp

--- refund/src/pages/SignIn.tsx
+++ refund/src/pages/SignIn.tsx
@@ -32,6 +32,13 @@ export function SignIn() {
       <Button type="submit" isLoading={isLoading}>
         Entrar
       </Button>
+
+      <a
+        href="/signup"
+        className="text-sm font-semibold text-gray-100 mt-5 mb-4 text-center hover:text-green-800 transition ease-linear border-2 border-transparent hover:border-green-800 py-2 rounded-lg"
+      >
+        Criar Contato
+      </a>
     </form>
   )
 }
