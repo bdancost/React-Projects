7d771c6c3afcffb8262804baaea84d9aa82d3f4b
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-12-16 13:07:57 -0300

    Criando a Página Dashboard

---
+++ refund/src/routes/ManagerRoutes.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router'
+
+import { Dashboard } from '../pages/Dashboard'
+import { NotFound } from '../pages/NotFound'
+
+import { AppLayout } from '../components/AppLayout'
+
+export function ManagerRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route path="/" element={<Dashboard />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
