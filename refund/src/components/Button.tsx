e7fc05cc3600e709f9a64546aa2ae96ceb15a09c
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-12-16 19:28:21 -0300

    Variantes do botao

--- refund/src/components/Button.tsx
+++ refund/src/components/Button.tsx
@@ -1,12 +1,27 @@
+import { classMerge } from '../utils/classMerge'
+
 type Props = React.ComponentProps<'button'> & {
   isLoading?: boolean
+  variant?: 'base' | 'icon' | 'iconSmall'
 }
 
-export function Button({ isLoading, children, type = 'button', ...rest }: Props) {
+const variants = {
+  button: {
+    base: 'h-12',
+    icon: 'h-12 w-12',
+    iconSmall: 'h-8 w-8',
+  },
+}
+
+export function Button({ isLoading, children, className, type = 'button', variant = 'base', ...rest }: Props) {
   return (
     <button
       type={type}
-      className="w-full h-12 bg-green-100 text-white font-semibold rounded-lg hover:bg-green-200 disabled:opacity-50 disabled:cursor-not-allowed transition-ease-linear flex items-center justify-center"
+      className={classMerge([
+        ' bg-green-100 text-white font-semibold rounded-lg hover:bg-green-200 disabled:opacity-50 disabled:cursor-not-allowed transition-ease-linear flex items-center justify-center',
+        variants.button[variant],
+        className,
+      ])}
       disabled={isLoading}
       {...rest}
     >
--- refund/src/pages/Dashboard.tsx
+++ refund/src/pages/Dashboard.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
+import searchSvg from '../assets/search.svg'
 import { Input } from '../components/Input'
+import { Button } from '../components/Button'
 
 export function Dashboard() {
   const [name, setName] = useState('')
@@ -19,6 +21,10 @@ export function Dashboard() {
         className="flex flex-1 items-center justify-between pb-6 border-b border-b-gray-400 md:flex-row gap-2 mt-6"
       >
         <Input placeholder="Pesquise pelo nome" onChange={(e) => setName(e.target.value)} />
+
+        <Button type="submit" variant="icon">
+          <img src={searchSvg} alt="Ícone de pesquisar" className="w-5" />
+        </Button>
       </form>
     </div>
   )
