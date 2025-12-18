afaa375aff5dae94553b4fd92ca96b2e4b0b7cd4
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-12-18 11:05:27 -0300

    Component de Loading

--- refund/src/components/AuthLayout.tsx
+++ refund/src/components/AuthLayout.tsx
@@ -3,8 +3,8 @@ import logoSvg from '../assets/logo.svg'
 
 export function AuthLayout() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center bg-gray-400 text-gray-100">
-      <main className="bg-gray-500 p-8 rounded-md flex items-center flex-col gap-6 md:min-w-[462px]">
+    <div className="w-screen h-screen flex flex-col items-center justify-center bg-gray-400 text-gray-100 p-8">
+      <main className="bg-gray-500 p-8 rounded-md flex items-center flex-col gap-6 w-full md:max-w-[462px]">
         <img src={logoSvg} alt="Logo" />
         <Outlet />
       </main>
