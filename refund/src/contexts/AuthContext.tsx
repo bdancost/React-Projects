39b0f985de44068e35c4195acdd631cfdc5b6d63
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-12-20 14:09:22 -0300

    Criando tipo do usuário

--- refund/src/contexts/AuthContext.tsx
+++ refund/src/contexts/AuthContext.tsx
@@ -1,7 +1,15 @@
+import { useState } from 'react'
+import type { ReactNode } from 'react'
 import { createContext } from 'react'
 
-export const AuthContext = createContext({})
+type AuthContext = {
+  session: null | UserAPIResponse
+}
 
-export function AuthProvider({ children }: { children: React.ReactNode }) {
-  return <AuthContext.Provider value={{ name: 'John Doe' }}>{children}</AuthContext.Provider>
+export const AuthContext = createContext({} as AuthContext)
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [session, setSession] = useState<UserAPIResponse | null>(null)
+
+  return <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
 }
