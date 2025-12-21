05f1e1d0646deae09913be804ecd46c748eeedcd
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-12-21 08:33:23 -0300

    Consultas e Paginaçao

--- refund/src/pages/Dashboard.tsx
+++ refund/src/pages/Dashboard.tsx
@@ -48,6 +48,11 @@ export function Dashboard() {
     }
   }
 
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    fetchRefunds()
+  }
+
   function handlePagination(action: 'next' | 'previous') {
     setPage((prevPage) => {
       if (action === 'next' && prevPage < totalOfPage) {
@@ -64,14 +69,14 @@ export function Dashboard() {
 
   useEffect(() => {
     fetchRefunds()
-  }, [])
+  }, [page])
 
   return (
     <div className="bg-gray-500 rounded-xl p-10 md:min-w-3xl">
       <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
 
       <form
-        onSubmit={fetchRefunds}
+        onSubmit={onSubmit}
         className="flex flex-1 items-center justify-between pb-6 border-b border-b-gray-400 md:flex-row gap-2 mt-6"
       >
         <Input placeholder="Pesquise pelo nome" onChange={(e) => setName(e.target.value)} />
